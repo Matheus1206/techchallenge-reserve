231c1c8f22f94a4269920f6d3a7e4319d71b3177
Matheus1206 <bandeiramatheus@hotmail.com>
2024-03-06 19:48:44 -0300

    feature: push parte 2

--- src/main/java/com/example/demo/repository/RestauranteRepository.java
+++ src/main/java/com/example/demo/repository/RestauranteRepository.java
@@ -1,8 +1,13 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.RestauranteModel;
+import com.example.demo.model.Restaurante;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 
-public interface RestauranteRepository extends MongoRepository<RestauranteModel, Long> {
+public interface RestauranteRepository extends MongoRepository<Restaurante, Long> {
+    @Query("{?0:?1}")
+    List<Restaurante> pesquisaPorParametro(String parametro, String valor);
 }
