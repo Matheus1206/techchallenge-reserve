f40b0f4c04044b86c11033f3c32f1ad253720845
Matheus1206 <bandeiramatheus@hotmail.com>
2024-03-26 06:12:10 -0300

    feat: Adicionando classe de testes

--- src/test/java/com/example/demo/cliente/ClienteRepositoryTest.java
+++ src/test/java/com/example/demo/cliente/ClienteRepositoryTest.java
@@ -1,6 +1,6 @@
-package com.example.demo.restaurante;
+package com.example.demo.cliente;
 
-import com.example.demo.repository.RestauranteRepository;
+import com.example.demo.repository.ClienteRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
@@ -8,14 +8,14 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
-class RestauranteRepositoryTest {
+class ClienteRepositoryTest {
+
     @Autowired
-    private RestauranteRepository restauranteRepository;
+    private ClienteRepository clienteRepository;
 
     @Test
     void devePermitirCriarTabela(){
-        var totalDeRegistros = restauranteRepository.count();
+        var totalDeRegistros = clienteRepository.count();
         assertThat(totalDeRegistros).isNotNegative();
     }
-
 }
--- src/test/java/com/example/demo/estabelecimento/EstabelecimentoServiceTest.java
+++ src/test/java/com/example/demo/estabelecimento/EstabelecimentoServiceTest.java
@@ -1,7 +1,9 @@
 package com.example.demo.estabelecimento;
 
+import com.example.demo.model.HorarioFuncionamento;
 import com.example.demo.model.Reserva;
 import com.example.demo.model.Restaurante;
+import com.example.demo.model.TipoCozinha;
 import com.example.demo.repository.ReservaRepository;
 import com.example.demo.repository.RestauranteRepository;
 import com.example.demo.services.EstabelecimentoService;
@@ -14,8 +16,13 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.context.ApplicationEventPublisher;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -43,5 +50,22 @@ class EstabelecimentoServiceTest {
         when(reservaRepository.findById("1234")).thenReturn(Optional.of(reserva));
         estabelecimentoService.avaliarReserva("1234");
         verify(eventPublisher, times(1)).publishEvent(any(ReservaConfirmadaEvent.class));
+    }
+
+    @Test
+    void testMudarStatusReserva(){
+       Reserva reserva = new Reserva("Teste", 45, LocalDateTime.now(), "teste", new Restaurante());
+       when(reservaRepository.findById("1234")).thenReturn(Optional.of(reserva));
+       Reserva reservaAlterada = estabelecimentoService.mudarStatusReserva("1234");
+       assertFalse(reservaAlterada.getStatusReserva());
+    }
+
+    @Test
+    void testListarReservas(){
+        List<Reserva> lista = new ArrayList<>();
+        lista.add(new Reserva("Teste", 45, LocalDateTime.now(), "teste", new Restaurante("Restaurante Teste", "SP", Arrays.asList(HorarioFuncionamento.JANTA), TipoCozinha.BRASILEIRA, 34)));
+        when(reservaRepository.findReservaRestauranteNome("Restaurante Teste")).thenReturn(lista);
+        List<Reserva> reservaList = estabelecimentoService.listarReservas("Restaurante Teste");
+        assertEquals("Teste", reservaList.get(0).getEmailCliente());
     }
 }
