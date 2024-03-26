18d1fa6d70e86a07e3d8222a5386c6d73c6cefac
Matheus1206 <bandeiramatheus@hotmail.com>
2024-03-26 07:06:45 -0300

    feat: Adicionando classe de testes da reserva com cenários de exception e cliente não encontrado

--- src/test/java/com/example/demo/reserva/ReservaServiceTest.java
+++ src/test/java/com/example/demo/reserva/ReservaServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.reserva;
 
 import com.example.demo.dto.ReservaDto;
+import com.example.demo.handler.QuantidadeDeMesaDisponivelException;
 import com.example.demo.model.*;
 import com.example.demo.repository.ClienteRepository;
 import com.example.demo.repository.ReservaRepository;
@@ -17,6 +18,7 @@ import java.util.Arrays;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -40,7 +42,7 @@ class ReservaServiceTest {
 
     @Test
     void testValidarUmaReservaService(){
-        ReservaDto reservaDto = new ReservaDto("teste@teste.com", 30, LocalDateTime.now().plusHours(2), "teste");
+        ReservaDto reservaDto = new ReservaDto("teste@teste.com", 3, LocalDateTime.now().plusHours(2), "teste");
         Restaurante restaurante = new Restaurante("Teste Restaurante", "SP", Arrays.asList(HorarioFuncionamento.JANTA), TipoCozinha.BRASILEIRA, 30);
         when(restauranteRepository.findById(any(String.class))).thenReturn(Optional.ofNullable(restaurante));
         Cliente cliente = new Cliente("Matheus", "teste@teste.com");
@@ -49,6 +51,20 @@ class ReservaServiceTest {
         when(reservaRepository.save(any(Reserva.class))).thenReturn(reserva);
         Reserva validaedReserva = reservaService.validaReserva(reservaDto, "Teste Restaurante");
         assertEquals("teste@teste.com", validaedReserva.getEmailCliente());
+    }
+
+    @Test
+    void testValidarUmaReservaComAQuantidadeMenorDeMesa(){
+        ReservaDto reservaDto = new ReservaDto("teste@teste.com", 3, LocalDateTime.now().plusHours(2), "teste");
+        Restaurante restaurante = new Restaurante("Teste Restaurante", "SP", Arrays.asList(HorarioFuncionamento.JANTA), TipoCozinha.BRASILEIRA, 0);
+        when(restauranteRepository.findById(any(String.class))).thenReturn(Optional.ofNullable(restaurante));
+        Cliente cliente = new Cliente("Matheus", "teste@teste.com");
+        when(clienteRepository.findByEmail(reservaDto.emailCliente())).thenReturn(Optional.ofNullable(cliente));
+        Reserva reserva = new Reserva(reservaDto.emailCliente(), reservaDto.lugares(), reservaDto.dataReserva(), reservaDto.observacao(),restaurante);
+        when(reservaRepository.save(any(Reserva.class))).thenReturn(reserva);
+        assertThrows(QuantidadeDeMesaDisponivelException.class, () ->{
+            reservaService.validaReserva(reservaDto, "Teste Restaurante");
+        });
     }
 
 }
